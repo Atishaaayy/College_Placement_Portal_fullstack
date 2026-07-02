ff8dbb1909e37fe32fdce0837370e54b35b54765
Atishaaayy <atishayjain937@gmail.com>
2026-07-02 10:54:35 +0530

    Configure CORS_ALLOWED_ORIGINS for Vercel deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,5 +119,6 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    "https://college-placement-portal-fullstack.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
